726692bc9ca008973da9cb4195b202cb3fe71ee3
dheerendra <dheerendrabhandari@gmail.com>
2025-06-15 08:35:44 +0000

    sc3bucket created

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "dheerendra-backend-2025"
+    bucket  = "dheerendraS-backend-2025"
     key     = "terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
