c8d049360601ba74a8949e79f848d62065241978
dheerendra <dheerendrabhandari@gmail.com>
2025-06-16 16:45:24 +0000

    new update

--- portForward.sh
+++ portForward.sh
@@ -1,3 +1,5 @@
+kubectl port-forward svc/test-app-service 30001:3001 --address 0.0.0.0 &
+
 helm upgrade --install prometheus prometheus-community/prometheus   --set server.service.type=NodePort   --set server.service.nodePort=30900 --set server.service.port=9090
 
 kubectl port-forward svc/prometheus-server 9090:80 --address 0.0.0.0 &
