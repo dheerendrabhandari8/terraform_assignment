31992cf824bc5c27cc4dffe1e7a2b75e054f18dd
dheerendra <dheerendrabhandari@gmail.com>
2025-06-16 16:00:50 +0000

    grafana

--- portForward.sh
+++ portForward.sh
@@ -1,6 +1,5 @@
 kubectl port-forward svc/test-app-service 3000:3000 --address 0.0.0.0 &
 
-kubectl port-forward svc/prometheus-server 9090:80 --address 0.0.0.0
-
+kubectl port-forward svc/prometheus-server 9090:80 --address 0.0.0.0 &
 
 kubectl port-forward svc/grafana 3000:3000 --address 0.0.0.0 &
