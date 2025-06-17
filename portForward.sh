3e0f6da8903a3d3ea5c6f9dd731f217e5214142f
dheerendra <dheerendrabhandari@gmail.com>
2025-06-17 05:22:12 +0000

    new-final

--- portForward.sh
+++ portForward.sh
@@ -1,18 +1,25 @@
-# 1. Expose test-app-service on port 3001
-kubectl port-forward svc/test-app-service 3000:31000 --address 0.0.0.0
+#!/bin/bash
 
-# 2. Deploy/Upgrade Prometheus with desired NodePort and service port
-#helm upgrade --install prometheus prometheus-community/prometheus \
- # --set server.service.type=NodePort \
- # --set server.service.nodePort=30900 \
- # --set server.service.port=9090
+# 1. Expose test-app-service using NodePort on 31000
+kubectl patch svc test-app-service \
+  -p '{"spec": {"type": "NodePort", "ports": [{"port": 3000, "targetPort": 3000, "nodePort": 31000}]}}'
 
-# 3. Expose Prometheus on port 9090 (forwards to service's internal port 9090)
-#kubectl port-forward svc/prometheus-server 9090:80 --address 0.0.0.0 &
-#
-#helm upgrade --install grafana grafana/grafana \
- # --set service.type=NodePort \
- # --set service.nodePort=30800 \
- # --set service.port=3000
+# 2. Deploy/Upgrade Prometheus with NodePort on 30900
+echo "Installing/upgrading Prometheus..."
+helm upgrade --install prometheus prometheus-community/prometheus \
+  --set server.service.type=NodePort \
+  --set server.service.nodePort=30900 \
+  --set server.service.port=9090
 
-#kubectl port-forward svc/grafana 30800:3000 --address 0.0.0.0 &
+# 3. Deploy/Upgrade Grafana with NodePort on 30800
+echo "Installing/upgrading Grafana..."
+helm upgrade --install grafana grafana/grafana \
+  --set service.type=NodePort \
+  --set service.nodePort=30800 \
+  --set service.port=3000
+
+
+kubectl port-forward svc/grafana 3001:3000 --address 0.0.0.0 &
+
+echo ""
+echo "✅ Services deployed and exposed via NodePorts:"
