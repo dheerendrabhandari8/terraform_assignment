93d091f5a21b0913d8e91f70361b2a15bb17b312
dheerendra <dheerendrabhandari@gmail.com>
2025-06-19 11:12:59 +0000

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+kubectl set image deployment/test-app-deployment test-app=dheerendrabhandari/nodejsapp:latest && kubectl rollout status deployment/test-app-deployment
+
+sleep 20
 cd /home/ubuntu/terraform_assignment/ && kubectl apply -f  ingress.yaml
 cd /home/ubuntu/terraform_assignment/ && kubectl apply -f  service.yaml
